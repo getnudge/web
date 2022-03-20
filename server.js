9d8bd34a3dc650cc2665ac00b3fc6892459140eb
Pranav Ramesh <laphatize@protonmail.com>
2022-03-20 00:42:51 -0400

    Update server.js

--- server.js
+++ server.js
@@ -24,9 +24,18 @@ app.get('/analytics', function(req, res){
 app.get('/api/analyze', async function(req, res){
 
     // wconsole.log(req.query.text)
+    const datsa2 = await openai.createCompletion("text-davinci-002", {
+        prompt: `There keywords that describe "${req.query.text}" are:`,
+        temperature: 0.1,
+        max_tokens: 30,
+        top_p: 1,
+        frequency_penalty: 0.8,
+        presence_penalty: 0,
+    })
+
 
     const datsa = await openai.createCompletion("text-davinci-002", {
-        prompt: `Are the words ben franklin, us related to history`,
+        prompt: `Are the words ${datsa2.data.choices[0].text} describing "${req.query.context}". Answer either true or false.`,
         temperature: 0.3,
         max_tokens: 60,
         top_p: 1,
@@ -35,7 +44,9 @@ app.get('/api/analyze', async function(req, res){
     })
 
 
-    res.send(`You gave: ${req.query.text}<br><br>Open AI Gave: ${datsa.data.choices[0].text}`);
+    console.log("AI SUMMARY:" + "\n" + datsa2.data.choices[0].text + "\n" + datsa.data.choices[0].text);
+    
+    res.send(datsa.data.choices[0].text.toLowerCase());
   
 });
 
