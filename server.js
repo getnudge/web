d74e4111e9b3f55b76e19ff624ad1dd173c07b6c
Pranav Ramesh <laphatize@protonmail.com>
2022-03-28 09:06:56 -0400

    article writer out now

--- server.js
+++ server.js
@@ -24,6 +24,60 @@ app.get('/dashboard', function(req, res){
 });
 
 // API ENDPOINTS 
+app.get('/api/article', async function(req, res){
+    console.log("Article Writer Requested")
+    const data1 = await openai.createCompletion("text-davinci-002", {
+        prompt: `Create a paragraph about the following:\n${req.query.o1}`,
+        temperature: 1.0,
+        max_tokens: 100,
+        top_p: 1,
+        frequency_penalty: 0.8,
+        presence_penalty: 0,
+    }).catch(err => console.log(err))
+
+    const data2 = await openai.createCompletion("text-davinci-002", {
+        prompt: `Create a paragraph about the following:\n${req.query.o2}`,
+        temperature: 1.0,
+        max_tokens: 100,
+        top_p: 1,
+        frequency_penalty: 0.8,
+        presence_penalty: 0,
+    })
+
+    const data3 = await openai.createCompletion("text-davinci-002", {
+        prompt: `Create a paragraph about the following:\n${req.query.o3}`,
+        temperature: 1.0,
+        max_tokens: 100,
+        top_p: 1,
+        frequency_penalty: 0.8,
+        presence_penalty: 0,
+    })
+
+    const data4 = await openai.createCompletion("text-davinci-002", {
+        prompt: `Create a paragraph about the following:\n${req.query.o4}`,
+        temperature: 1.0,
+        max_tokens: 100,
+        top_p: 1,
+        frequency_penalty: 0.8,
+        presence_penalty: 0,
+    })
+
+    console.log(data1)
+    let serverResponse = {
+        o1: data1.data.choices[0].text,
+        o2: data2.data.choices[0].text,
+        o3: data3.data.choices[0].text,
+        o4: data4.data.choices[0].text,
+    }
+
+    res.send(serverResponse);
+});
+
+
+
+
+
+
 app.get('/api/analyze', async function(req, res){
 
     console.log("hit")
